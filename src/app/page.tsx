c2a3b724e3ee314091b1dbc0c73e02bb319bdd03
Otto Hellwig <ottohellwig@hotmail.com>
2024-02-16 10:07:30 +0800

    Init header/navbar, setup NextUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,76 @@
+'use client'
+import { useEffect } from 'react'
 import Image from 'next/image'
 import {Button} from '@nextui-org/button'; 
+import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/react";
+import React from 'react';
+import Link from 'next/link';
+
+function Test() {
+  console.log("TEST")
+}
 
 export default function Home() {
+  useEffect(() => {
+    console.log('in useEffect')
+  })
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "About Me",
+    "Experience",
+    "Projects",
+    "Contact",
+  ];
   return (
-    <main>
-      <div>
-      <Button>Click me</Button>
-      </div>
-    </main>
-  )
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <p className="font-bold text-inherit">OTTO</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+About Me
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#" aria-current="page">
+       Experience
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
 }
